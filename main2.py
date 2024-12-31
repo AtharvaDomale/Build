df1ff4420aa66a1eab5ce966f5b3f589cdd27e74
Atharva suresh Domale <atharvadomale@gmail.com>
2024-12-31 14:32:05 +0530

    changes in files

--- main2.py
+++ main2.py
@@ -13,3 +13,36 @@ recognizer = sr.Recognizer()
 speaker = tts.init()
 speaker.setProperty('rate', 150)
 
+
+def basic_function():
+    global recognizer
+
+
+
+    speaker.say("what you need to order")
+    speaker.runAndWait()
+
+    done = False
+
+    while not done:
+        try :
+
+            with sr.Microphone() as mic:
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                note = recognizer.recognize_google(audio)
+                note = note.lower()
+
+                print(note)
+
+                speaker.say("you said" + note)
+                speaker.runAndWait()
+
+
+
+assistant = GenericAssistant('intents.json')
+assistant.train_model()
+
+
