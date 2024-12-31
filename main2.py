e16cdc477a781598cfb5ce1cc3f88f0e29fd0eae
Atharva suresh Domale <atharvadomale@gmail.com>
2024-12-31 14:51:08 +0530

    changes in files

--- main2.py
+++ main2.py
@@ -35,12 +35,53 @@ def basic_function():
                 note = recognizer.recognize_google(audio)
                 note = note.lower()
 
-                print(note)
-
-                speaker.say("you said" + note)
+                speaker.say("choose a file  ")
                 speaker.runAndWait()
 
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
 
+                filname = recognizer.recognize_google(audio)
+                filname = filname.lower()
+            
+            with open(filname, 'w') as f:
+                f.write(note)
+                done = True
+                speaker.say("i sucessfully created the note{filename}")
+                speaker.runAndWait()
+
+        except sr.UnknownValueError:
+            recognizer = sr.Recognizer()
+            speaker.say("i did not understand you, please try again")
+            speaker.runAndWait()
+
+def add_todo():
+
+    global recognizer
+
+    speaker.say("what do you want to add to your todo list")
+    speaker.runAndWait()
+
+    done = False
+
+
+    while not done:
+
+        try:
+
+            with sr.Microphone() as mic:
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+                item = recognizer.recognize_google(audio)
+                item = item.lower()
+
+                todo_list.append(item)
+                done = True
+
+                speaker.say(f"i added {item} to the to do list")
+                speaker.runAndWait()
 
 assistant = GenericAssistant('intents.json')
 assistant.train_model()
