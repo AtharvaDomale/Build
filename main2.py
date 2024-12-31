4f5e523a1ebe73ce7b6e244b5cb84c1fcff92305
Atharva suresh Domale <atharvadomale@gmail.com>
2024-12-31 14:23:24 +0530

    changes in files

--- main2.py
+++ main2.py
@@ -6,3 +6,10 @@ import speech_recognition as sr
 import pyttsx3 as tts
 
 from neuralintents import GenericAssistant
+
+
+recognizer = sr.Recognizer()
+
+speaker = tts.init()
+speaker.setProperty('rate', 150)
+
